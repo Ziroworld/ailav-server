9dafa4b6f9e3e268ba8ff72af988d4d1cda215c7
meinkampf.369 <ziro0x0x0@gmail.com>
2024-12-22 08:02:46 +0545

    added logic of findbyid, deletebyid, update for user(customer), and updated the routes too.

--- controller/userController.js
+++ controller/userController.js
@@ -40,4 +40,44 @@ const createUser = async (req, res) => {
     }
 };
 
-module.exports = { findAllUser, createUser };
+const findById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    }
+    catch (e) {
+        console.error('Error finding users:', e.message);
+        res.status(500).json({ message: e.message });
+    }
+};
+
+const deleteById = async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(204).json("Data has been deleted.");
+    }
+    catch (e) {
+        console.error('Error deleting user:', e.message);
+        res.status(500).json({ message: e.message });
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(201).json(updatedUser);
+    }
+    catch (e) {
+        console.error('Error updating user:', e.message);
+        res.status(500).json({ message: e.message });
+    }
+};
+
+
+module.exports = { 
+    findAllUser, 
+    createUser,
+    findById,
+    deleteById,
+    update,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { findAllUser, createUser } = require('../controller/userController');
+const { findAllUser, createUser, findById, deleteById, update } = require('../controller/userController');
 const { userValidation } = require('../validation/userValidator');
 // const { generateToken , authenticateToken } = require('../security/userSecurity');
 
@@ -9,6 +9,8 @@ router.get('/user', findAllUser, (req, res) => {
     res.send('User route is working!');
 });
 router.post("/save", userValidation,createUser);
-
+router.delete("/:id", deleteById);
+router.get("/:id", findById);
+router.put("/:id",update);
 
 module.exports = router;
